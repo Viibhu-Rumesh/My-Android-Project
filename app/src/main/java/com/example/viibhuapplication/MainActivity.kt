b90cf8f451e8cda841530ebad3c76c0b2f330169
Viibhu <viibhurumesh@gmail.com>
2024-11-14 21:11:48 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/viibhuapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.viibhuapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val ButtonA = findViewById<Button>(R.id.Main_ButtonA_Button)
+        ButtonA.callOnClick()
+    }
+
+}
